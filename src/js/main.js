dea870b304e3dc395b9697b360f4161d2fbab154
Francuski Miroslav <francuski.miroslav@gmail.com>
2020-11-29 22:34:09 +0100

    added header and light and dark theme classes

--- src/js/main.js
+++ src/js/main.js
@@ -1,82 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
-	let timer;
-	let deleteFirstPhotoDelay;
-
-	async function start() {
-		try {
-			const response = await fetch('https://dog.ceo/api/breeds/list/all');
-			const data = await response.json();
-			createBreedList(data.message);
-		} catch (e) {
-			console.log('There was a problem fetching the breed list.');
-		}
+	// function to set a given theme/color-scheme
+	function setTheme(themeName) {
+		localStorage.setItem('theme', themeName);
+		document.documentElement.className = themeName;
 	}
 
-	start();
-
-	function createBreedList(breedList) {
-		document.getElementById('breed').innerHTML = `
-  <select id="breeds">
-        <option>Choose a dog breed</option>
-        ${Object.keys(breedList)
-					.map(function (breed) {
-						return `<option>${breed}</option>`;
-					})
-					.join('')}
-      </select>
-  `;
-
-		let selectBreed = document.querySelector('#breeds');
-		selectBreed.addEventListener('change', (e) => {
-			async function loadByBreed() {
-				if (e.target.value != 'Choose a dog breed') {
-					const response = await fetch(
-						`https://dog.ceo/api/breed/${e.target.value}/images`
-					);
-					const data = await response.json();
-					createSlideshow(data.message);
-				}
-			}
-
-			loadByBreed();
-		});
-	}
-
-	function createSlideshow(images) {
-		let currentPosition = 0;
-		clearInterval(timer);
-		clearTimeout(deleteFirstPhotoDelay);
-
-		if (images.length > 1) {
-			document.getElementById('slideshow').innerHTML = `
-  <div class="slide" style="background-image: url('${images[0]}')"></div>
-  <div class="slide" style="background-image: url('${images[1]}')"></div>
-  `;
-			currentPosition += 2;
-			if (images.length == 2) currentPosition = 0;
-			timer = setInterval(nextSlide, 3000);
+	// function to toggle between light and dark theme
+	function toggleTheme() {
+		if (localStorage.getItem('theme') === 'theme-dark') {
+			setTheme('theme-light');
 		} else {
-			document.getElementById('slideshow').innerHTML = `
-  <div class="slide" style="background-image: url('${images[0]}')"></div>
-  <div class="slide"></div>
-  `;
-		}
-
-		function nextSlide() {
-			document
-				.getElementById('slideshow')
-				.insertAdjacentHTML(
-					'beforeend',
-					`<div class="slide" style="background-image: url('${images[currentPosition]}')"></div>`
-				);
-			deleteFirstPhotoDelay = setTimeout(function () {
-				document.querySelector('.slide').remove();
-			}, 1000);
-			if (currentPosition + 1 >= images.length) {
-				currentPosition = 0;
-			} else {
-				currentPosition++;
-			}
+			setTheme('theme-dark');
 		}
 	}
+
+	// Immediately invoked function to set the theme on initial load
+	(function () {
+		if (localStorage.getItem('theme') === 'theme-dark') {
+			setTheme('theme-dark');
+		} else {
+			setTheme('theme-light');
+		}
+	})();
 });
