07253395660b16ceb34bbf58ad0fa2cbe1a3f3f2
Francuski Miroslav <francuski.miroslav@gmail.com>
2020-12-10 23:35:55 +0100

    fetched data and displayed it

--- src/js/main.js
+++ src/js/main.js
@@ -32,4 +32,61 @@ document.addEventListener('DOMContentLoaded', function () {
 	toggleThemeBtn.addEventListener('click', (e) => {
 		toggleTheme();
 	});
+
+	const countriesUrl = 'https://restcountries.eu/rest/v2/all';
+	const countriesContainer = document.getElementById('countriesContainer');
+
+	function getCountriesData() {
+		return new Promise((resolve, reject) => {
+			fetch(countriesUrl)
+				.then((resp) => {
+					if (!resp.ok) {
+						throw Error(`${resp.statusText} - ${resp.url}`);
+					}
+					return resp.json();
+				})
+				.then((data) => resolve(data))
+				.catch((err) => reject(err));
+		});
+	}
+
+	async function displayCountriesNumbers() {
+		const countriesList = await getCountriesData();
+
+		countriesContainer.innerHTML = countriesList
+			.map((country) => {
+				console.log(country);
+
+				const { name, population, region, capital, flag } = country;
+
+				return `
+					<div class="box">
+						<div class="box__top">
+							<img src="${flag}" alt="flag of a ${name}" />
+						</div>
+
+						<div class="box__bottom">
+							<h3 class="box__title">${name}</h3>
+							<ul class="box__info-list">
+								<li class="box__info-list-item">
+									<span class="box__info-list-item--bold">Populaiton:</span>
+									${population}
+								</li>
+								<li class="box__info-list-item">
+									<span class="box__info-list-item--bold">Region:</span> ${region}
+								</li>
+								<li class="box__info-list-item">
+									<span class="box__info-list-item--bold">Capitol:</span> ${capital}
+								</li>
+							</ul>
+						</div>
+					</div>
+					`;
+			})
+			.join('');
+	}
+
+	(function () {
+		displayCountriesNumbers();
+	})();
 });
